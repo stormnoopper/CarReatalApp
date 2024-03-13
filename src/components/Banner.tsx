59339137fdaa7b3d4908cc664a7a4a992f3d3992
saroot areerattanawetch <saroot.nae@gmail.com>
2024-03-13 11:00:34 +0700

    Assigment3

---
+++ src/components/Banner.tsx
@@ -0,0 +1,18 @@
+import style from './banner.module.css'
+import Image from 'next/image'
+
+export default function Banner (){
+    return(
+        <div className={style.banner}>
+            <Image src={'/img/cover.jpg'}
+            alt='cover'
+            fill={true}
+            priority
+            objectFit='cover'/>
+            <div className={style.bannerText}>
+                <h1>Your Travel Partner</h1>
+                <h3>Explore Your World with Us</h3>
+            </div>
+        </div>
+    )
+}
---
+++ src/components/ProductCard.tsx
@@ -0,0 +1,17 @@
+import styles from './productcard.module.css'
+import Image from 'next/image';
+
+export default function ProductCard({ carName,imgSrc }:{ carName:string , imgSrc:string }){
+    return(
+        <div className={styles.card}>
+            <div className={styles.cardimg}>
+                <Image src={imgSrc}
+                alt='Product Picture'
+                fill={true}
+                objectFit='cover'
+                />
+            </div>
+            <div className={styles.cardtext}>{carName}</div>
+        </div>
+    );
+}
