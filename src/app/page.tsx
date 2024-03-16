93bdd78952bf20b459c630eba9c3bff9a1d9abf0
saroot areerattanawetch <saroot.nae@gmail.com>
2024-03-16 13:18:16 +0700

    Assigment6

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,11 @@
-import Image from "next/image";
-import styles from "./page.module.css";
 import Banner from "@/components/Banner";
-import ProductCard from "@/components/ProductCard";
+import CarPanel from "@/components/CarPanel";
 
 export default function Home() {
   return (
     <main>
       <Banner/>
-      <div style={{margin:"20px",display:"flex", flexDirection:"row",flexWrap:"wrap", justifyContent:"space-around", alignContent:"space-around"}}>
-      <ProductCard carName="Honda Civic" imgSrc="/img/civic.jpg"/>
-      <ProductCard carName="Honda Accord" imgSrc="/img/accord.jpg"/>
-      <ProductCard carName="Toyota Fortuner" imgSrc="/img/fortuner.jpg"/>
-      <ProductCard carName="Tesla Model 3" imgSrc="/img/tesla.jpg"/>
-      </div>
+      <CarPanel/>
     </main>
   );
 }
