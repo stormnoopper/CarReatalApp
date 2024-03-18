cb0bac4068998c7931437ded206517e255207914
saroot areerattanawetch <saroot.nae@gmail.com>
2024-03-18 20:04:42 +0700

    FromAssigment08

--- src/components/CarPanel.tsx
+++ src/components/CarPanel.tsx
@@ -1,10 +1,14 @@
 'use client'
-import { useReducer } from "react"
+import { useReducer,useRef } from "react"
 import ProductCard from "./ProductCard"
 import Link from "next/link"
 
 export default function CarPanel(){
     
+    let count = useRef(0);
+    const inputRef = useRef<HTMLInputElement>(null);
+
+
     const compareReducer = (compareList:Set<string>, action:{type:string, carName:string}) =>{
         switch(action.type){
             case 'add' :{
@@ -41,6 +45,23 @@ export default function CarPanel(){
             <div className="w-full text-xl font-medium">Compare List: {compareList.size}</div>
                 {Array.from(compareList).map( (car)=><div key={car}onClick={()=>
                 dispatchCompare({type:'remove',carName:car})}>{car}</div>)}
+
+            <button className="block rounded-md bg-sky-600 hover:bg-indigo-600 
+            px-3 py-2 shadow-sm text-white" onClick={()=>{count.current=count.current+1;alert(count.current)}}>
+                Count with local variable
+            </button> 
+            <input type="text" placeholder='Please Fill' className="block text-grey-900 
+            text-sm rounded-lg p-2 m-2 bg-purple-50 ring-1 ring-inset ring-purple-400
+            focus:outline-none focus:bg-purple-200 focus:ring-2" ref={inputRef} />
+
+            <button className="block rounded-md bg-sky-600 hover:bg-indigo-600 
+            px-3 py-2 shadow-sm text-white"
+            onClick={()=>{
+                if(inputRef.current!=null){
+                    inputRef.current.focus();
+                }}}>
+                Focus Text Fill
+            </button> 
         </div>
     )
 }
